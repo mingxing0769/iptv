33f4f1e012d76b9025303219cb5a18375635a67f
mingxing0769 <124433897+mingxing0769@users.noreply.github.com>
2025-09-29 17:49:23 +0800

    Update mergeclean.py

--- mergeclean.py
+++ mergeclean.py
@@ -39,8 +39,7 @@ EPG_URL = "http://drewlive24.duckdns.org:8081/merged2_epg.xml.gz"
 OUTPUT_FILE = "out/MergedCleanPlaylist.m3u8"
 
 output_dir = os.path.dirname(OUTPUT_FILE)
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+os.makedirs(os.path.dirname(OUTPUT_FILE), exist_ok=True)
 
 # --- 功能开关 ---
 # 是否开启 URL 可用性检测 (会显著增加运行时间)
@@ -319,3 +318,4 @@ if __name__ == "__main__":
     print(f"\n✨ Merging complete at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}.")
 
     print(f"⏱️ Total execution time: {end_time - start_time:.2f} seconds.")
+
