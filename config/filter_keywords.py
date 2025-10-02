38e3b095b6c626ac544587c226ec7e4c02e3e087
mingxing0769 <124433897+mingxing0769@users.noreply.github.com>
2025-10-02 23:05:06 +0800

    Delete config/filter_keywords.py

--- config/filter_keywords.py
+++
@@ -1,3 +0,0 @@
-KEYWORDS = ["sports", "espn", "fox", "f1", "football", "news", "cnn","bbc", "abc", "sky"]
-nsfw_keywords = ['nsfw', 'xxx', 'porn', 'adult']
-indicators = [r'\bFHD\b', r'\bHD\b', r'\bSD\b', r'\bUHD\b', r'\b4K\b', r'\b2K\b', r'\b8K\b']
