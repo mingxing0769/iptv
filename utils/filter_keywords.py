d8f0c5372f038b7032f8587e6bf8fc5677018f61
mingxing0769 <124433897+mingxing0769@users.noreply.github.com>
2025-10-02 22:28:35 +0800

    Create filter_keywords.py

---
+++ utils/filter_keywords.py
@@ -0,0 +1,3 @@
+Category_Key = ["sports", "espn", "fox", "f1", "football", "news", "cnn", "bbc", "abc", "sky"]
+Nsfw_Key = ['nsfw', 'xxx', 'porn', 'adult']
+Indicators_key = [r'\bFHD\b', r'\bHD\b', r'\bSD\b', r'\bUHD\b', r'\b4K\b', r'\b2K\b', r'\b8K\b']
