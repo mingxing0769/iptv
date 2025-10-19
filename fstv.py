556674898e7d604cc55f37e82bf591e981da7c29
mingxing0769 <124433897+mingxing0769@users.noreply.github.com>
2025-10-19 08:47:33 +0800

    Update fstv.py

--- fstv.py
+++ fstv.py
@@ -143,7 +143,7 @@ async def fetch_fstv_channels():
     async with async_playwright() as p:
         browser = await p.firefox.launch(headless=True)
         context = await browser.new_context(
-            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0"
+            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:141.0) Gecko/20100101 Firefox/141.0"
         )
         await context.set_extra_http_headers({
             "origin": "https://fstv.space",
