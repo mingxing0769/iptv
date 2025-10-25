abd92c159edef2b568c0934cb11ce75a95bdeef9
mingxing0769 <124433897+mingxing0769@users.noreply.github.com>
2025-10-25 12:41:25 +0800

    Update sources_urls.py

--- config/sources_urls.py
+++ config/sources_urls.py
@@ -3,6 +3,6 @@ playlist_urls = [
     "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/DaddyLiveEventsRAW.m3u8",    
     "https://raw.githubusercontent.com/Drewski2423/DrewLive/main/TVPass.m3u",    
     "https://raw.githubusercontent.com/Drewski2423/DrewLive/main/AriaPlus.m3u8",    
-    "https://raw.githubusercontent.com/mingxing0769/iptv/refs/heads/main/out/FSTV.m3u8",    
+    # "https://raw.githubusercontent.com/mingxing0769/iptv/refs/heads/main/out/FSTV.m3u8",    
     "https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/A1x.m3u8"
 ]
