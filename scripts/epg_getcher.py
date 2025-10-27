d5e77adc40c81efc8e511fa47825c6f4ec33d1a6
mingxing0769 <124433897+mingxing0769@users.noreply.github.com>
2025-10-27 12:39:09 +0800

    Update epg_getcher.py

--- scripts/epg_getcher.py
+++ scripts/epg_getcher.py
@@ -19,7 +19,8 @@ PROJECT_ROOT = os.path.dirname(SCRIPT_DIR)
 OUT_DIR = os.path.join(PROJECT_ROOT, "out")
 
 # EPG 源地址
-EPG_URL = "http://drewlive24.duckdns.org:8081/DrewLive.xml.gz"
+# EPG_URL = "http://drewlive24.duckdns.org:8081/DrewLive.xml.gz"
+EPG_URL = "https://tinyurl.com/DrewLiveEPG24"
 
 # 定义输入和输出文件路径
 PLAYLIST_PATH = os.path.join(OUT_DIR, "MergedCleanPlaylist.m3u8")
