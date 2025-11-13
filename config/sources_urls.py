48599fb1ccff03debce901d40e6842cfa0feea51
mingxing0769 <124433897+mingxing0769@users.noreply.github.com>
2025-11-13 17:07:35 +0800

    Update sources_urls.py

--- config/sources_urls.py
+++ config/sources_urls.py
@@ -1,4 +1,5 @@
 playlist_urls = [   
    "https://aria.bnkd.xyz/aria.m3u",
    "https://aria.bnkd.xyz/aria+.m3u"
+   "https://raw.githubusercontent.com/mingxing0769/iptv/refs/heads/main/out/tvpass.m3u8"
 ]
