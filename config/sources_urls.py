32e07a225396b50952632f5ae53c7fd1727baad2
mingxing0769 <124433897+mingxing0769@users.noreply.github.com>
2026-02-15 15:16:56 +0800

    Add new playlist URL to sources_urls.py

--- config/sources_urls.py
+++ config/sources_urls.py
@@ -2,5 +2,6 @@ playlist_urls = [
    "https://aria.bnkd.xyz/aria.m3u",
    "https://aria.bnkd.xyz/aria+.m3u",
    "http://tvpass.org/playlist/m3u",
+   "https://raw.githubusercontent.com/vbskycn/iptv/refs/heads/master/tv/iptv4.m3u",
    "https://iptv-org.github.io/iptv/index.m3u"
 ]
